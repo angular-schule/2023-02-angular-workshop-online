b0e0dfefd57110a8f649edf476e424863519870c
Ferdinand Malcher <ferdinand@malcher.media>
2023-02-23 11:35:19 +0100

    Events

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
@@ -8,4 +8,18 @@ import { Book } from '../shared/book';
 })
 export class BookComponent {
   @Input() book?: Book;
+  @Output() rateUp = new EventEmitter<Book>();
+  @Output() rateDown = new EventEmitter<Book>();
+
+  doRateUp() {
+    if (this.book) {
+      this.rateUp.emit(this.book);
+    }
+  }
+
+  doRateDown() {
+    if (this.book) {
+      this.rateDown.emit(this.book);
+    }
+  }
 }
--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -27,4 +27,12 @@ export class DashboardComponent {
       }
     ];
   }
+
+  doRateUp(book: Book) {
+    console.log('UP', book);
+  }
+
+  doRateDown(book: Book) {
+    console.log('DOWN', book);
+  }
 }
