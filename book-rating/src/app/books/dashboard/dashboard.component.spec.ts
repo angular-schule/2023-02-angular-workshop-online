0c4f9da75509a259af6964f1344640bcdcad0adc
Ferdinand Malcher <ferdinand@malcher.media>
2023-02-23 15:19:26 +0100

    Shallow Component Test

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
@@ -8,12 +9,16 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      imports: [],
+      providers: [],
+      schemas: [NO_ERRORS_SCHEMA] // Shallow Component Test
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+    // Element: fixture.nativeElement
     fixture.detectChanges();
   });
 
